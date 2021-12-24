fb879a0d8c56a9ee13d0d2cbdf7d7b25a4514623
Rye Miller <rmiller@corra.com>
2021-12-24 09:43:37 -0700

    Update to location of project files for namespace consistency.

---
+++ src/Helper/Data.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Robust location and GPS library for Magento 2.
+ *
+ * @category  Iods
+ * @version   000.1.0
+ * @author    Rye Miller <rye@drkstr.dev>
+ * @copyright Copyright (c) 2021, Rye Miller (https://ryemiller.io)
+ * @license   MIT (https://en.wikipedia.org/wiki/MIT_License)
+ */
+declare(strict_types=1);
+
+namespace Iods\Tulip\Helper;
+
+class Data
+{
+    // ...
+}
